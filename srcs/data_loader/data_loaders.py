04b2fc7a0639690d2559d5821edb6e249cb0c32c
Sidenko-M-E <matvey.sidenko@mail.ru>
2025-01-31 15:56:12 +0700

    Add augs. Upd req.

--- srcs/data_loader/data_loaders.py
+++ srcs/data_loader/data_loaders.py
@@ -4,13 +4,14 @@ from typing import List
 import cv2
 import numpy as np
 import torch
+import albumentations as A
 from torch.utils.data import DataLoader, Dataset
 
 
 class SignDataset(Dataset):
     def __init__(self, paths: List[Path], transform=None):
         self.paths = paths
-        self.transform = transform # если есть аугментации
+        self.transform = transform
 
         labels = sorted(set(str(x).split('/')[-2] for x in paths))
         self.one_hot_encoding = {label: i for i, label in enumerate(labels)}
@@ -21,6 +22,11 @@ class SignDataset(Dataset):
     def __getitem__(self, idx):
         image = cv2.imread(str(self.paths[idx]))
         label = str(self.paths[idx]).split('/')[-2]
+
+        # Add augmentations
+        if self.transform is not None:
+            image = self.transform(image=image)['image']
+        
         image = cv2.resize(image, (200, 200))
         image = np.transpose(image, (2, 0, 1))
 
@@ -30,8 +36,23 @@ class SignDataset(Dataset):
 def get_sign_dataloader(
         path_train, path_val, batch_size, shuffle=True, num_workers=1,
     ):
-    train_dataset = SignDataset(paths=[*Path(path_train).rglob('*.jpg')])
-    val_dataset = SignDataset(paths=[*Path(path_val).rglob('*.jpg')])
+    transform = A.Compose([
+        A.ShiftScaleRotate(shift_limit=(-.1, .1),
+                           scale_limit=(-.2, .2),
+                           rotate_limit=0,
+                           p=1),
+        A.OneOf([
+            A.RandomToneCurve(scale=0.1, 
+                              p=0.2),
+            A.RandomBrightnessContrast(brightness_limit=(-0.2, 0.2), 
+                                       contrast_limit=(-0.2, 0.2),
+                                       p=0.8),
+        ], p=0.3),                 
+        A.GaussNoise(p=0.2),
+        A.Normalize()])
+
+    train_dataset = SignDataset(paths=[*Path(path_train).rglob('*.jpg')], transform=transform)
+    val_dataset = SignDataset(paths=[*Path(path_val).rglob('*.jpg')], transform=transform)
 
     loader_args = {
         'batch_size': batch_size,
