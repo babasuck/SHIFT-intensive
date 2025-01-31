02be86ff238cf054fc3072fb405682e7ecf0a29f
Ivan Ovsyannikov <karanreaderer@gmail.com>
2025-01-31 12:49:24 +0000

    stable

--- srcs/model/model.py
+++ srcs/model/model.py
@@ -11,3 +11,13 @@ def get_efficientnet_model(num_classes: int = 29):
     model = torchvision.models.efficientnet_b0(weights=torchvision.models.EfficientNet_B0_Weights.DEFAULT)
     model.classifier[-1] = torch.nn.Linear(model.classifier[-1].in_features, num_classes)
     return model
+
+def get_vit_model(num_classes: int = 29):
+    model = torchvision.models.vit_b_16(weights=torchvision.models.ViT_B_16_Weights.DEFAULT)
+    model.heads.head = torch.nn.Linear(model.heads.head.in_features, num_classes)
+    return model
+
+def get_efficientnetv2_model(num_classes: int = 29):
+    model = torchvision.models.efficientnet_v2_s(weights=torchvision.models.EfficientNet_V2_S_Weights.DEFAULT)
+    model.classifier[-1] = torch.nn.Linear(model.classifier[-1].in_features, num_classes)
+    return model
